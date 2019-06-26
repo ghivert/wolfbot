ff0fae60d697084b0e8026ae7a3771b874d5b742
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-06-26 22:55:41 +0200

    :construction: Read port from env variable

--- src/Main.hs
+++ src/Main.hs
@@ -2,15 +2,28 @@
 module Main where
 
 import Web.Scotty (scotty, get, html, notFound, status, text, middleware)
+import Text.Read (readMaybe)
 import Text.Blaze.Html.Renderer.Text
 import Network.HTTP.Types.Status
 import Network.Wai.Middleware.Static
+import System.Environment
 import Views.Homepage
+import qualified Views.NotFound as NotFound
 
-main :: IO ()
-main = scotty 5000 $ do
+startServer :: Int -> IO ()
+startServer port = scotty port $ do
   middleware $ staticPolicy (addBase "public")
   get "/" $ html $ renderHtml homepage
   notFound $ do
     status notFound404
-    text "404 Page Not Found"
+    html $ renderHtml NotFound.notFound
+
+errorMessage :: String -> IO ()
+errorMessage = putStrLn . (++) "Unable to start the server on port: "
+
+main :: IO ()
+main = do
+  stringPort <- getEnv "PORT"
+  let printError = errorMessage stringPort
+      port = readMaybe stringPort in
+    maybe printError startServer port
